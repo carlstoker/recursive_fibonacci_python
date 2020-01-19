8d7365d399d91c4baa8fb56ff017c80852f82a7b
carlstoker <40079934+carlstoker@users.noreply.github.com>
2020-01-19 18:09:03 -0500

    Return a list rather than printing the sequence.

--- recursive_fibonacci.py
+++ recursive_fibonacci.py
@@ -11,20 +11,22 @@ def fibonacci(desired, num_1=0, num_2=1):
         num_1 -- First component integer.
         num_2 -- Second component integer.
     """
-    print(num_1, end=' ')
+    sequence = [num_1]
 
     if (num_1 + num_2) < desired:
         # Recursively pass new numbers to fibonacci function if the
         # sum of num_1 and num_2 are less than the desired integer.
-        fibonacci(desired, num_2, num_1 + num_2)
+        sequence.extend(fibonacci(desired, num_2, num_1 + num_2))
     else:
-        print(num_2)
+        sequence.append(num_2)
+
+    return sequence
 
 
 def main():
     desired = int(input("Enter desired number: "))
-    fibonacci(desired)
-
+    formatted_sequence = ', '.join(str(i) for i in fibonacci(desired))
+    print('{}.'.format(formatted_sequence))
 
 if __name__ == '__main__':
     main()
